f3733e882697e69e1773211c35def0ab8161b03c
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-04 14:51:23 -0800

    Create data post request

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var data = require('./routes/data');
 
 var app = express();
+
+data_array = []
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,12 +20,13 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/data', data);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/data.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res) {
+  console.log("************** POST REQUEST *************");
+  data_array.push(req.body);
+  for (var i = 0; i < data_array.length; i++) {
+    console.log(data_array[i]);
+  }
+  res.send("hi");
+});
+
+module.exports = router;
