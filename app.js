6aa59b9f3d0166822bdf5310fcfce34df9b3e779
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-06 09:34:45 -0800

    Change data route name to api

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var data = require('./routes/data');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/data', data);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
