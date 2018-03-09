910c3985c851caa91e6767e38092f192b8ef3857
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-09 13:36:53 -0800

    Sweeping changes

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,19 @@ var router = express.Router();
 
 router.post('/', function(req, res) {
   console.log("************** POST REQUEST *************");
-  data_array.push(req.body);
+  var entry = req.body;
+  var date = new Date();
+  var datetime = date.getDate() + "/"
+                 + (date.getMonth()+1)  + "/"
+                 + date.getFullYear() + " @ "
+                 + date.getHours() + ":"
+                 + date.getMinutes();
+
+  //entry['status'] = req.body;
+  entry['timestamp'] = datetime;
+  data_array.push(entry);
   for (var i = 0; i < data_array.length; i++) {
-    console.log(data_array[i]);
+    console.log(entry);
   }
 
   // make db query to update data vis
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res, next) {
                         distance: '0' });
 });
 
+router.get('/history', function(req, res, next) {
+  res.render('history', { title: 'History | Senior Signal'});
+});
+
 module.exports = router;
