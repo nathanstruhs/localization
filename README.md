a07f0530ad41ef2f9d946b7f13f5722193211374
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-09 12:14:01 -0800

    Update readme, create homescreen

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## CS 447 - Final Project
 Tyler Radabaugh, Nathan Struhs, Brad Cook
 
-[https://stormy-wave-48927.herokuapp.com/](https://stormy-wave-48927.herokuapp.com/)
+[https://senior-signal.herokuapp.com/](https://senior-signal.herokuapp.com/)
 
 Commands
 --------
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Senior Signal' });
+  res.render('index', { title: 'Senior Signal',
+                        location: 'At home',
+                        distance: '0' });
 });
 
 module.exports = router;
