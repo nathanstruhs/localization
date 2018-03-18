b5c0ec473d543bc4fd41c19243b8f60589e93eab
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-17 19:16:43 -0700

    Format history entries

--- helpers/date-helper.js
+++ helpers/date-helper.js
@@ -1,11 +1,11 @@
 module.exports = {
   getCurrentDateAndTime: function() {
     var date = new Date();
-    var datetime = date.getDate() + "/"
-                   + (date.getMonth()+1)  + "/"
-                   + date.getFullYear() + " @ "
+    var datetime = (date.getMonth()+1) + "/"
+                   + date.getDate() + "/"
+                   + date.getFullYear() + " at "
                    + date.getHours() + ":"
-                   + (date.getMinutes() < 10 ? '0' : '') +  date.getMinutes();
+                   + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
     return datetime;
   }
 }
